1e5d8931513933508ada5493def9caede2f10894
Simon de Lang <simondelang@gmail.com>
2019-02-28 21:36:13 +0100

    Run using command testrunner

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,21 +2,19 @@
 // Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/vuejs.md#vuejs
 module.exports = function(config) {
   config.set({
-    files: ["my-name.vue", "test/setup.js", "test/my-name.spec.js"],
     mutate: ["my-name.vue"],
     mutator: "vue",
-    testRunner: "mocha",
+    testRunner: "command",
+    commandRunner: { command: 'npm run mocha' },
     testFramework: 'mocha',
-    transpilers: ['webpack'],
     mochaOptions: {
-      files: ['dist/test.bundle.js'],
-      ui: 'bdd',
-      opts: 'test/mocha.opts'
+      files: ['dist/test.bundle.js']
     },
+    transpilers: ['webpack'],
     webpack: {
       configFile: 'webpack.test.config.js'
     },
-    reporters: ["progress", "clear-text"],
+    reporters: ["progress", "clear-text", "html"],
     coverageAnalysis: "off"
   });
 };
